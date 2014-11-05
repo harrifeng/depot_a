b71ddfa17528df320267e37b73aa18fa05113b2e
hfeng <haoran.feng@sap.com>
2014-11-05 01:01:17 -0800

    use 'rails new depot -d mysql' to create a application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '5ac5f27e474b617689d6638d62b7f7db8030a9e1ac31dec71267a7128d3c482c6d5acd8eda28a917aa590fc947628bb4caee2f0336cd0b36c64b24557d3b83f7'
